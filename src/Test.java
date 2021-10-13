f123339f12dc20b5a1eef6ad8454a04dbeea2a70
huangcaixia <euphoria.hcx@gmail.com>
2021-10-13 15:52:02 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * 〈一句话功能简述〉<br>
+ *
+ * @author huangcaixia
+ * @create 2021-10-13 15:37
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
